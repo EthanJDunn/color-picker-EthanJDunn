d9cea19ef2a27af96e9d35ae4facad70b8df8362
EthanJDunn <edunn9@csu.fullerton.edu>
2024-05-16 16:05:48 -0700

    Bugfixing

--- app/src/main/java/com/example/colorpickerfix/MyViewModel.kt
+++ app/src/main/java/com/example/colorpickerfix/MyViewModel.kt
@@ -30,71 +30,36 @@ class MyViewModel : ViewModel() {
     fun loadInputs(act: MainActivity) {
         viewModelScope.launch {
             prefs.redSeekBarProgress.collectLatest { value ->
-                if (value.isNotEmpty()) {
                     act.redseekBar.progress = value.toInt()
-                }
             }
         }
         viewModelScope.launch {
             prefs.redSwitch.collectLatest { value ->
-                if (value.isNotEmpty()) {
                     act.redSwitch.isChecked = value.toBoolean()
                     act.redseekBar.isEnabled = value.toBoolean()
-                    act.redseekBar.visibility = if (value.toBoolean()) View.VISIBLE else View.INVISIBLE
-                }
-            }
-        }
-        viewModelScope.launch {
-            prefs.redText.collectLatest { value ->
-                if (value.isNotEmpty()) {
-                    act.redText.setText(value)
-                }
             }
         }
         viewModelScope.launch {
             prefs.blueSeekBarProgress.collectLatest { value ->
-                if (value.isNotEmpty()) {
                     act.blueseekBar.progress = value.toInt()
-                }
             }
         }
         viewModelScope.launch {
             prefs.blueSwitch.collectLatest { value ->
-                if (value.isNotEmpty()) {
                     act.blueSwitch.isChecked = value.toBoolean()
                     act.blueseekBar.isEnabled = value.toBoolean()
-                    act.blueseekBar.visibility = if (value.toBoolean()) View.VISIBLE else View.INVISIBLE
-                }
-            }
-        }
-        viewModelScope.launch {
-            prefs.blueText.collectLatest { value ->
-                if (value.isNotEmpty()) {
-                    act.blueText.setText(value)
-                }
             }
         }
         viewModelScope.launch {
             prefs.greenSeekBarProgress.collectLatest { value ->
-                if (value.isNotEmpty()) {
                     act.greenseekBar.progress = value.toInt()
-                }
+
             }
         }
         viewModelScope.launch {
             prefs.greenSwitch.collectLatest { value ->
-                if (value.isNotEmpty()) {
                     act.greenSwitch.isChecked = value.toBoolean()
                     act.greenseekBar.isEnabled = value.toBoolean()
-                    act.greenseekBar.visibility = if (value.toBoolean()) View.VISIBLE else View.INVISIBLE
-                }
-            }
-        }
-        viewModelScope.launch {
-            prefs.greenText.collectLatest { value ->
-                if (value.isNotEmpty()) {
-                    act.greenText.setText(value)
-                }
             }
         }
     }
